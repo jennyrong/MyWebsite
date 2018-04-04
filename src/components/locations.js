9433f39bc0e78a32dacd52386f45111997debda1
jennyrong <janetrong123@gmail.com>
2018-04-04 10:45:48 -0700

    commit locations

--- src/components/locations.js
+++ src/components/locations.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-//import GoogleMap from './google_map';
+import GoogleMap from './google_map';
 import Counter from './counter';
 
 export default class Locations extends Component {
     render(){
         return(
             <div>
-                <div className="heading">Locations 3</div>
+                <div className="heading">Locations</div>
                 
-                <Counter />
+                <div><GoogleMap /></div>
                 <Link to="/index">
                    Back to index page
                 </Link>
