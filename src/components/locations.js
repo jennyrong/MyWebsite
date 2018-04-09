14f51cca2fce1afae9084b77fdc5536d372af6a3
jennyrong <janetrong123@gmail.com>
2018-04-10 06:55:34 +0800

    commit locations

--- src/components/locations.js
+++ src/components/locations.js
@@ -7,7 +7,7 @@ export default class Locations extends Component {
     render(){
         return(
             <div>
-                <div className="heading">Locations</div>
+                <div className="heading">Locations 1</div>
                 
                 <div><GoogleMap /></div>
                 <Link to="/index">
